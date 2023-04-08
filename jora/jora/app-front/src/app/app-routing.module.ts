8c1d5fa902d35212f8c5e5f9b4abdee75d55a1a7
diananik958 <diananikkel958@gmail.com>
2023-04-08 16:49:00 +0700

    front

--- jora/jora/app-front/src/app/app-routing.module.ts
+++ jora/jora/app-front/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {RegisterPageComponent} from "./register-page/register-page.component";
+import {MainPageComponent} from "./main-page/main-page.component";
+import {ProjectPageComponent} from "./project-page/project-page.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "login", component: LoginPageComponent},
+  {path: "register", component: RegisterPageComponent},
+  {path: "main", component: MainPageComponent},
+  {path: "project", component: ProjectPageComponent},
+  {path: "**", component: LoginPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- jora/jora/app-front/src/app/app.module.ts
+++ jora/jora/app-front/src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import {CreateTaskDialog, MainPageComponent} from './main-page/main-page.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    MainPageComponent,
+    ProjectPageComponent,
+    CreateTaskDialog
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatTableModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
