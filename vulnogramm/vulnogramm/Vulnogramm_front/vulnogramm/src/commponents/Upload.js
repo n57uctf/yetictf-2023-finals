6abea5d1f71ed4df681cff95ca32b514c377d88a
avsjanka <99408523+avsjanka@users.noreply.github.com>
2023-04-13 18:49:21 +0700

    Update Upload.js

--- vulnogramm/vulnogramm/Vulnogramm_front/vulnogramm/src/commponents/Upload.js
+++ vulnogramm/vulnogramm/Vulnogramm_front/vulnogramm/src/commponents/Upload.js
@@ -1,17 +1,16 @@
 import React, { handleChangeFile, handleFile, useState } from 'react'
-import axios from 'axios';
+
 
 export default function Home ({setPhoto}){
 
 
 
-  handleFile = (e) => {      
-    const content = e.target.result;
-    console.log(content);
+  function handleFile(e){      
+    let content = e.target.result;
     setPhoto(content);
   }
     
-  handleChangeFile = (file) => {
+  function handleChangeFile(file){
       let fileData = new FileReader();
       fileData.onloadend = handleFile;
       fileData.readAsDataURL(file); 
@@ -22,7 +21,7 @@ export default function Home ({setPhoto}){
     return (
       <div >
           <form onSubmit={e => this.submit(e)}>  
-            <input type="file" accept="image/png,image/bmp,image/svg" id="upload" hidden  onChange={e => handleChangeFile(e.target.files[0])}></input>
+            <input type="file" accept="image/png" id="upload" hidden  onChange={e => handleChangeFile(e.target.files[0])}></input>
             <label id="upload_label" for="upload" className="button">Choose your photo</label>
           </form> 
       </div>
